fbced2a6f34f88a7d3d49387105cfa97e0f24616
sadia-pao <aandiyumbo@gmail.com>
2024-11-13 13:16:51 -0500

    comentario

---
+++ src/main/java/com/mycompany/anahiandi2020/AnahiAndi2020.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.anahiandi2020;
+
+/**
+ *
+ * @author SO-LAB1-PC21
+ */
+public class AnahiAndi2020 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
